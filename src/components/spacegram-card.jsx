6e338d52e0688b6fe04a7171eb450e54d9bb4b3d
Bashir <b_egeh@hotmail.com>
2021-09-19 00:24:51 -0400

    add state to control Like button when clicked

--- src/components/spacegram-card.jsx
+++ src/components/spacegram-card.jsx
@@ -10,9 +10,10 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export default function SpaceGram() {
-  const [loading, setLoading] = useState(false);
   const [data, setData] = useState();
-  const { hdurl, explanation, title, date } = data;
+  const { hdurl, explanation, title, date } = data || {};
+
+  const [liked, setLiked] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -20,36 +21,48 @@ export default function SpaceGram() {
 
   // Retrieve date from NASA api
   const fetchData = async () => {
-    setLoading(true);
     await axios
       .get(`https://api.nasa.gov/planetary/apod`, {
         params: { api_key: process.env.REACT_APP_NASA_API_KEY },
       })
       .then((res) => setData(res.data));
-    setLoading(false);
   };
 
   return (
-    data && (
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia component="img" image={hdurl} alt={title} />
-        <CardContent>
-          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+    <Card sx={{ maxWidth: 345 }}>
+      {data && (
+        <>
+          <CardMedia component="img" image={hdurl} alt={title} />
+          <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {title}
             </Typography>
-            <Typography variant="body1" color="text.secondary" style={{display: "flex", alignItems: "center", justifyContent: 'center'}}>
+            <Typography
+              variant="body1"
+              color="text.secondary"
+              style={{
+                margin: "10px 0px",
+              }}
+            >
               {date}
             </Typography>
-          </div>
-          <Typography variant="body2" color="text.secondary" textAlign="left">
-            {explanation}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Like</Button>
-        </CardActions>
-      </Card>
-    )
+            <Typography variant="body2" color="text.secondary" textAlign="left">
+              {explanation}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              size="small"
+              style={{textTransform: "none", fontSize: "16px"}}
+              color={liked ? "primary" : "inherit"}
+              onClick={() => setLiked(!liked)}
+            >
+              Like
+            </Button>
+          </CardActions>
+        </>
+      )}
+    </Card>
   );
 }
