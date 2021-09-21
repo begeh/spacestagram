ca89fb13ad99df3c4088b5605a62f1746e22ece4
Bashir <b_egeh@hotmail.com>
2021-09-21 17:12:47 -0400

    refactor css and html formatting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./fonts/style.css";
 import "./App.css";
-import MainPage from './pages/MainPage';
+import MainPage from "./pages/MainPage";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div className="container">
+      <header className="header">
+        <Typography className="title" variant="h3" component="div">
+          Spacestagram
+        </Typography>
+        <Typography className="subtitle" variant="h6">
+          Powered By NASA's Image API
+        </Typography>
+      </header>
+      <MainPage />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,6 @@
 import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 import ImageCard from "../components/ImageCard";
-import Typography from "@mui/material/Typography";
 import { getImages } from "../helpers";
 
 export default function MainPage() {
@@ -49,15 +48,7 @@ export default function MainPage() {
 
   return (
     <div className="main-page">
-      <header className="main-header">
-        <Typography className="main-title" variant="h3" component="div">
-          Spacestagram
-        </Typography>
-        <Typography className= "main-subtitle" variant="h6">Powered By NASA's Image API</Typography>
-      </header>
-      <body className="main-body">
-        <PageView />
-      </body>
+      <PageView />
     </div>
   );
 }
